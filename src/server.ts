f9441ec33ffa703ab07efb65d632bd18f751d129
MusayevDoniyor <doniyormusayev2009@gmail.com>
2025-03-24 14:00:03 +0500

    initial code structure

---
+++ src/server.ts
@@ -0,0 +1,14 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+
+dotenv.config();
+const app = express();
+
+app.use(cors());
+app.use(express.json());
+
+const PORT = process.env.PORT;
+app.listen(PORT, () => {
+  console.log(`🚀 Server is running on http://localhost:${PORT}`);
+});
