03ba1ade52831bc8ffc0856eceac2b2a940b9a36
MusayevDoniyor <doniyormusayev2009@gmail.com>
2025-03-24 16:31:40 +0500

    Enhance API with additional metadata and middleware; added keywords, author, and description in package.json. Implemented helmet and morgan for security and logging in server.ts, along with error handling for API routes.

--- src/server.ts
+++ src/server.ts
@@ -1,8 +1,15 @@
-import express from "express";
+import express, {
+  ErrorRequestHandler,
+  Request,
+  Response,
+  NextFunction,
+} from "express";
 import dotenv from "dotenv";
 import cors from "cors";
 import connectDB from "./config/db.js";
 import productsRoute from "./routes/products.routes.js";
+import helmet from "helmet";
+import morgan from "morgan";
 
 dotenv.config();
 const app = express();
@@ -10,7 +17,22 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+app.use(helmet());
+app.use(morgan("dev"));
+
 app.use("/api/v1/products", productsRoute);
+
+app.use((req: Request, res: Response, next: NextFunction) => {
+  res.status(404).json({ message: "API route not found" });
+});
+
+app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
+  console.error("❌ Error:", err.message);
+
+  res
+    .status(500)
+    .json({ message: "Internal Server Error", error: err.message });
+});
 
 connectDB();
 
